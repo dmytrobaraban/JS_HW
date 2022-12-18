b1cc1fba92450767b68f95f202438743863e3b19
Dmytro Baraban <79997173+dmytrobaraban@users.noreply.github.com>
2022-12-18 18:38:34 +0200

    add Task_3

--- index.js
+++ index.js
@@ -40,3 +40,28 @@ for (let j = 0; j < arr.length; j += len){
 console.log(result)
 
 */
+
+/*
+
+// Task_3
+
+const namesOfDays = {
+    ua: ['Понеділок', 'Вівторок', 'Середа', 'Четвер', "П'ятниця", 'Субота', 'Неділя'],
+    en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+}
+
+function getNameOfDay(lang, day) {
+  if (day === 0 || day > 7) {
+    alert ('Error! Enter a day number from 1 to 7 !')
+  }
+  if (lang === 'ua') {
+    day = namesOfDays.ua[day - 1];
+  } else if (lang === 'en')
+    day = namesOfDays.en[day - 1];
+   // lang === 'ua' ? day = namesOfDays.ua[day - 1] : day = namesOfDays.en[day - 1]; // version 2
+  return day;
+}
+
+console.log(getNameOfDay('ua', 7))
+
+*/
