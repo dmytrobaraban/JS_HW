f36dae9bbd33fa3a30cbb6771035cca85ee638ed
Dmytro Baraban <79997173+dmytrobaraban@users.noreply.github.com>
2022-12-18 17:19:09 +0200

    add Task_2

--- index.js
+++ index.js
@@ -20,3 +20,23 @@ for (const key in citiesAndCountries){
 console.log(result) 
 
 */
+
+// Task_2
+
+/*
+
+const amount = 12;
+const len = 3;
+const result = [];
+const arr = [];
+
+for (let i = 1; i <= amount; i++){
+ arr[arr.length] = i;
+}
+for (let j = 0; j < arr.length; j += len){
+  result[result.length] = arr.slice(j, j + len);
+}
+
+console.log(result)
+
+*/
