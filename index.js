87e91d8865720436e048f03a2042eb29b361f49c
Dmytro Baraban <79997173+dmytrobaraban@users.noreply.github.com>
2023-01-04 00:12:04 +0200

    add exersise 2

--- index.js
+++ index.js
@@ -1,88 +1,50 @@
-// 1.
+// Exersise 2
 
-/*
-
-for (let i = 1; i <= 10; i++){
-  if (i % 2 === 0 && i % 3 !== 0 ){
-    console.log('Fiz');
-  } else if (i % 3 === 0){
-    console.log('FizBuz');
-  } else {
-    console.log('Buz');
+class CustomString {
+  
+  constructor(string){
+    this.string = string;
   }
-}
-
-*/
-
-// 2.
-
-/*
-
-const fact = 7
-let result = 1;
-for (let i = 1; i <= fact; i++){
-  result *= i
-}
-
-console.log(res)
-
-*/
-
-// 3.
-
-/*
-
-const sheetsInReamPaper = 500;
-const consumptionPerWeek = 1200;
-const weeksAmount = 8;
-const reamsOfPaperPerPeriod = consumptionPerWeek * weeksAmount / sheetsInReamPaper;
-const ifResultNotInteger = Number( Math.trunc(reamsOfPaperPerPeriod) + 1);
-
-if (Number.isInteger(reamsOfPaperPerPeriod) === false){
-  console.log(ifResultNotInteger);
-} else {
-  console.log(reamsOfPaperPerPeriod);
-}
-
-*/
-
-// 4.
-
-/*
-
-function roomAndFloorNumber(roomNumber){
-    const roomsOnFloor = 3;
-    const floors = 9;
-    let floor = 0;
-    const roomOnPorch = roomsOnFloor * floors;
-    let porch = roomNumber / roomOnPorch;
-    if (!Number.isInteger(porch)){
-      floor = ( roomNumber - ( Number( Math.trunc(porch) ) * roomOnPorch ) ) / roomsOnFloor;
-    } if (!Number.isInteger(floor)) {
-      floor = Number( Math.trunc(floor) ) + 1;
+  
+  reverse(string){
+    let result = '';
+    for (let i = 0; i < string.length; i++){
+      result = string[i] + result;
     }
-    if (!Number.isInteger(porch)){
-      porch = Number( Math.trunc(porch) + 1)
+    return result;
+  }
+  
+  ucFirst(string){
+    const arr = [];
+    for (let i = 0; i < string.length; i++){
+      if (i === 0){
+       arr.push(string[i].toUpperCase());
+       continue;
+      }
+      arr.push(string[i]);
     }
-    console.log(`The room in ${porch} porch and on ${floor} floor.`)
+    return arr.join('');
   }
-
-roomAndFloorNumber(444)
-
-*/
-
-// 5.
-
-/*
-
-function buildPiramide(mediana){
-  for (let i = 0; i < mediana; i++){
-    let dash = '-'.repeat(mediana - i - 1);
-    let thorp = '#'.repeat(i * 2 + 1);
-    console.log(`${dash}${thorp}${dash}`)
+  
+  ucWords(string){
+    const arr = [];
+    for (let i = 0; i < string.length; i++){
+      if (i === 0){
+       arr.push(string[i].toUpperCase());
+       continue;
+      } else if (string[i - 1] === ' '){
+        arr.push(string[i].toUpperCase());
+        continue;
+      }
+      arr.push(string[i]);
+    }
+    return arr.join('');
   }
+  
 }
 
-buildPiramide(6)
-
-*/
+// Для перевірки
+const myString = new CustomString();
+console.log('reverse() =>', myString.reverse('qwerty'));
+console.log('ucFirst() =>', myString.ucFirst('qwerty'));
+console.log('ucWords() =>', myString.ucWords('qwerty qwerty qwerty'));
