a2b73af11382caee5dcd4e50dc49c9b49e554fc9
Dmytro Baraban <79997173+dmytrobaraban@users.noreply.github.com>
2023-01-11 14:42:29 +0200

    complite hw4

--- index.js
+++ index.js
@@ -1,3 +1,118 @@
+// Exersise 1
+
+const studentArr = [
+  {
+    name: 'Сергей',
+    surname: 'Войлов',
+    ratingPoint: 1000,
+    schoolPoint: 1000,
+    course: 2,
+  },
+  {
+    name: 'Татьяна',
+    surname: 'Коваленко',
+    ratingPoint: 880,
+    schoolPoint: 700,
+    course: 1,
+  },
+  {
+    name: 'Анна',
+    surname: 'Кугир',
+    ratingPoint: 1430,
+    schoolPoint: 1200,
+    course: 3,
+  },
+  {
+    name: 'Станислав',
+    surname: 'Щелоков',
+    ratingPoint: 1130,
+    schoolPoint: 1060,
+    course: 2,
+  },
+  {
+    name: 'Денис',
+    surname: 'Хрущ',
+    ratingPoint: 1000,
+    schoolPoint: 990,
+    course: 4,
+  },
+  {
+    name: 'Татьяна',
+    surname: 'Капустник',
+    ratingPoint: 650,
+    schoolPoint: 500,
+    course: 3,
+  },
+  {
+    name: 'Максим',
+    surname: 'Меженский',
+    ratingPoint: 990,
+    schoolPoint: 1100,
+    course: 1,
+  },
+  {
+    name: 'Денис',
+    surname: 'Марченко',
+    ratingPoint: 570,
+    schoolPoint: 1300,
+    course: 4,
+  },
+  {
+    name: 'Антон',
+    surname: 'Завадский',
+    ratingPoint: 1090,
+    schoolPoint: 1010,
+    course: 3,
+  },
+  {
+    name: 'Игорь',
+    surname: 'Куштым',
+    ratingPoint: 870,
+    schoolPoint: 790,
+    course: 1,
+  },
+  {
+    name: 'Инна',
+    surname: 'Скакунова',
+    ratingPoint: 1560,
+    schoolPoint: 200,
+    course: 2,
+  },
+];
+
+class Student {
+  constructor(enrolee) {
+    this.id = Student.ID++;
+    this.isSelfPayment = true;
+    Object.assign(this, enrolee);
+
+    Student.listOfStudents.push(this);
+    Student.filterBySelfPayment();
+  }
+
+  static ID = 1;
+  static listOfStudents = [];
+
+  static filterBySelfPayment() {
+    const studentsList = Student.listOfStudents;
+    studentsList.sort((a, b) => {
+      if (b.ratingPoint > a.ratingPoint) return 1;
+      if (b.ratingPoint < a.ratingPoint) return -1;
+      if (b.ratingPoint === a.ratingPoint) {
+        b.schoolPoint > a.schoolPoint ? 1 : 0;
+      }
+    });
+    studentsList.slice(0, 5).forEach((e) => (e.isSelfPayment = false));
+    studentsList.slice(5).forEach((e) => (e.isSelfPayment = true));
+  }
+}
+
+for (const enrolee of studentArr) {
+  new Student(enrolee);
+}
+
+console.log(Student.listOfStudents);
+
 // Exersise 2
 
 class CustomString {
